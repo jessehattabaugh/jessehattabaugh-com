3e4cef6e862fa35f3c7a379fe29569e3c599c98c
Jesse Hattabaugh <arkanciscan@gmail.com>
2018-08-24 20:53:04 -0700

    diff works
    now when you run the test if the new and old shots are the same it creates a new file with the differences highlighted

--- test/home.js
+++ test/home.js
@@ -1,39 +1,86 @@
-const Lab = require('lab');
-const lab = exports.lab = Lab.script();
+const Lab = require("lab");
+const lab = (exports.lab = Lab.script());
 const { describe, it, before, after } = lab;
 
-const Code = require('code');
+const Code = require("code");
 const expect = Code.expect;
 
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-const looksSame = require('looks-same');
+const looksSame = require("looks-same");
+
+const fs = require("fs-extra");
 
 let browser;
 let page;
 
-describe('the home page', () => {
+describe("the home page", () => {
+	before(async () => {
+		browser = await puppeteer.launch({
+			executablePath: "chrome.exe",
+			headless: false
+			//ignoreHTTPSErrors: true,
+			//dumpio: true,
+		});
+	});
 
-    before(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            executablePath: 'chrome.exe'
-        });
-        page = await browser.newPage();
-    });
+	it("looks right on mobile", async () => {
+		page = await browser.newPage();
+		page.setViewport({
+			width: 360,
+			height: 640,
+			deviceScaleFactor: 1,
+			isMobile: true,
+			hasTouch: true,
+			isLandscape: false
+		});
+		await page.goto("http://localhost:1234");
 
-    it('looks right on mobile', async () => {
-        await page.goto('http://localhost:1234');
-        await page.screenshot({ path: 'shots/home.png' });
-        await new Promise((resolve) => {
-            looksSame('shots/home.png', 'shots/home.png', (err, eql) => {
-                expect(eql, 'return value of looks-same').to.be.true();
-                resolve();
-            });
-        });
-    });
+		const newPath = "shots/home.new.png";
+		const oldPath = "shots/home.old.png";
+		const diffPath = "shots/home.diff.png";
 
-    after(async () => browser.close());
+		await page.screenshot({
+			path: newPath,
+			fullscreen: true
+		});
 
+		const oldFileExists = await fs.pathExists(oldPath);
+
+		if (oldFileExists) {
+			const looksSameOpts = {
+				current: newPath,
+				diff: diffPath,
+				highlightColor: "#ff00ff",
+				ignoreAntialiasing: true,
+				ignoreCaret: true,
+				reference: oldPath,
+				//strict: true,
+				tolerance: 2.3,
+			};
+			const eql = await new Promise(resolve => {
+				looksSame(newPath, oldPath, looksSameOpts, (err, eql) => {
+					if (err) throw err;
+					resolve(eql);
+				});
+			});
+			if (!eql) {
+				await new Promise(resolve => {
+					looksSame.createDiff(looksSameOpts, err => {
+						if (err) throw err;
+						resolve();
+					});
+				});
+			}
+			expect(eql, "return value of looks-same").to.be.true();
+		} else {
+			await fs.move(newPath, oldPath);
+			expect(
+				oldFileExists,
+				"a previous screenshot was not found, current screenshot has been saved"
+			).to.be.true();
+		}
+	});
+
+	after(async () => browser.close());
 });
-
