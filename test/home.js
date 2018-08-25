87e57fe140abba71e03eaad65d006b6c6bc340ca
Jesse Hattabaugh <arkanciscan@gmail.com>
2018-08-24 21:15:27 -0700

    formatting

--- test/home.js
+++ test/home.js
@@ -1,30 +1,30 @@
-const Lab = require("lab");
+const Lab = require('lab');
 const lab = (exports.lab = Lab.script());
-const { describe, it, before, after } = lab;
+const {describe, it, before, after} = lab;
 
-const Code = require("code");
+const Code = require('code');
 const expect = Code.expect;
 
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-const looksSame = require("looks-same");
+const looksSame = require('looks-same');
 
-const fs = require("fs-extra");
+const fs = require('fs-extra');
 
 let browser;
 let page;
 
-describe("the home page", () => {
+describe('the home page', () => {
 	before(async () => {
 		browser = await puppeteer.launch({
-			executablePath: "chrome.exe",
-			headless: false
+			executablePath: 'chrome.exe',
+			headless: false,
 			//ignoreHTTPSErrors: true,
 			//dumpio: true,
 		});
 	});
 
-	it("looks right on mobile", async () => {
+	it('looks right on mobile', async () => {
 		page = await browser.newPage();
 		page.setViewport({
 			width: 360,
@@ -32,17 +32,17 @@ describe("the home page", () => {
 			deviceScaleFactor: 1,
 			isMobile: true,
 			hasTouch: true,
-			isLandscape: false
+			isLandscape: false,
 		});
-		await page.goto("http://localhost:1234");
+		await page.goto('http://localhost:1234');
 
-		const newPath = "shots/home.new.png";
-		const oldPath = "shots/home.old.png";
-		const diffPath = "shots/home.diff.png";
+		const newPath = 'shots/home.new.png';
+		const oldPath = 'shots/home.old.png';
+		const diffPath = 'shots/home.diff.png';
 
 		await page.screenshot({
 			path: newPath,
-			fullscreen: true
+			fullscreen: true,
 		});
 
 		const oldFileExists = await fs.pathExists(oldPath);
@@ -51,33 +51,33 @@ describe("the home page", () => {
 			const looksSameOpts = {
 				current: newPath,
 				diff: diffPath,
-				highlightColor: "#ff00ff",
+				highlightColor: '#ff00ff',
 				ignoreAntialiasing: true,
 				ignoreCaret: true,
 				reference: oldPath,
 				//strict: true,
 				tolerance: 2.3,
 			};
-			const eql = await new Promise(resolve => {
+			const eql = await new Promise((resolve) => {
 				looksSame(newPath, oldPath, looksSameOpts, (err, eql) => {
 					if (err) throw err;
 					resolve(eql);
 				});
 			});
 			if (!eql) {
-				await new Promise(resolve => {
-					looksSame.createDiff(looksSameOpts, err => {
+				await new Promise((resolve) => {
+					looksSame.createDiff(looksSameOpts, (err) => {
 						if (err) throw err;
 						resolve();
 					});
 				});
 			}
-			expect(eql, "return value of looks-same").to.be.true();
+			expect(eql, 'return value of looks-same').to.be.true();
 		} else {
 			await fs.move(newPath, oldPath);
 			expect(
 				oldFileExists,
-				"a previous screenshot was not found, current screenshot has been saved"
+				'a previous screenshot was not found, current screenshot has been saved',
 			).to.be.true();
 		}
 	});
