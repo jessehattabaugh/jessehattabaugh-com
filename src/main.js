6c7dff4fe1f8f3fc8e8cc7a35640469228c24d2d
Jesse Hattabaugh <arkanciscan@gmail.com>
2018-09-05 14:20:05 -0700

    adding sw plugin

--- src/main.js
+++ src/main.js
@@ -1,17 +1,2 @@
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('sw.js').then(
-			(registration) => {
-				console.log(
-					'ServiceWorker registration successful with scope: ',
-					registration.scope,
-				);
-			},
-			(err) => {
-				console.log('ServiceWorker registration failed: ', err);
-			},
-		);
-	});
-}
 
 console.log("Welcome to my website!");
--- src/sw.js
+++
@@ -1,2 +0,0 @@
-console.log('hello world, I am at your service');
-workbox.precaching.precacheAndRoute([]);
