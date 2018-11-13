a03a6b3fc2a07b9c1ea16614e9944da330782703
Jesse Hattabaugh <arkanciscan@gmail.com>
2018-11-12 22:08:19 -0800

    improving the size of webp and quality of jpeg

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 console.log('Welcome to my website!');
+
+// registering the SW after DOM Load to protect the page load from the SW
+document.addEventListener('load', () => {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker
+			.register('sw.js')
+			.then((reg) => {
+				console.log(
+					'Registration successful, scope is:',
+					reg.scope,
+				);
+				reg.onupdatefound = (ev) =>
+					console.log('sw update found', ev);
+			})
+			.catch((er) => {
+				console.log(
+					'Service worker registration failed, error:',
+					er,
+				);
+			});
+	} else {
+		console.info(
+			"The current browser doesn't support service workers",
+		);
+	}
+});
