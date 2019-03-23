e90a04cb107743b21f62db2287433754048cf615
Jesse Hattabaugh <arkanciscan@gmail.com>
2019-03-23 16:08:33 -0700

    removing duplicate config from prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,8 @@
 module.exports = {
 	arrowParens: 'always',
 	bracketSpacing: false,
-	endOfLine: 'lf',
 	jsxBracketSameLine: false,
-	printWidth: 80,
 	semi: true,
 	singleQuote: true,
-	tabWidth: 4,
 	trailingComma: 'all',
-	useTabs: true,
 };
