1f23088ccc31250a06f3ddfdbedf7fe1a9766d90
Jesse Hattabaugh <webmaster@jessehattabaugh.com>
2019-07-28 14:11:24 -0700

    i don't know man

---
+++ functions/share.js
@@ -0,0 +1,1 @@
+exports.handler = (request) => console.log('howdy world', request);
---
+++ src/main.js
@@ -0,0 +1,20 @@
+//import 'babel-polyfill';
+
+console.log('Welcome to my website yo!');
+
+// registering the SW after DOM Load to protect the page load from the SW
+window.addEventListener('load', () => {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker
+			.register('sw.js')
+			.then((reg) => {
+				console.log('Registration successful, scope is:', reg.scope);
+				reg.onupdatefound = (ev) => console.log('sw update found', ev);
+			})
+			.catch((er) => {
+				console.log('Service worker registration failed, error:', er);
+			});
+	} else {
+		console.info("The current browser doesn't support service workers");
+	}
+});
