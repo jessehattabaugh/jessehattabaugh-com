8f076de2e8326e3d3b7b988c5c52da61c824a4b6
Jesse Hattabaugh <arkanciscan@gmail.com>
2020-01-17 15:05:09 -0800

    workbox cacheing
    brings the lighthouse pwa score up to 93!

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,4 @@
-oninstall = (ev) => console.info('sw installed');
-onactivate = (ev) => console.log('sw activated');
-onfetch = (ev) => console.info("sw fetched");
-onmessage = (ev) => console.log('sw message', ev);
-onnotificationclick = (ev) => console.log('sw notification click', ev);
-onnotificationclose = (ev) => console.log('sw notification close', ev);
-onpush = (ev) => console.log('sw push:', ev);
-onpushsubscriptionchange = (ev) => console.log('sw push subscription change', ev);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-console.info('sw evaluated');
+// workbox injectManifest
+workbox.precaching.precacheAndRoute([]);
---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	globDirectory: 'dist/',
+	globPatterns: ['**/*.{css,js,ico,html,png,webp,webmanifest}'],
+	swDest: 'dist/sw.js',
+	swSrc: 'src/sw.js',
+};
