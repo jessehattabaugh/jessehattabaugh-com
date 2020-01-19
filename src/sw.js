055cafcee383ea94024f5c5637a915110af09cdb
Jesse Hattabaugh <arkanciscan@gmail.com>
2020-01-19 10:23:53 -0800

    reinstalled workbox with a backgroundSync on share

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,19 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts(
+	'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
+);
+
+// precache all the static files w/ workbox-cli injectManifest
+workbox.precaching.precacheAndRoute([]);
+
+// handle POSTS to the share form with backgroundSync
+workbox.routing.registerRoute(
+	/share.html/,
+	new workbox.strategies.NetworkOnly({
+		plugins: [
+			new workbox.backgroundSync.Plugin('shareQueue', {
+				maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+			}),
+		],
+	}),
+	'POST',
+);
