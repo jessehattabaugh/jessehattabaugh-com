88d91e0acd171197c75ceda78fc513fe3bfc7811
Jesse Hattabaugh <arkanciscan@gmail.com>
2020-01-24 17:56:45 -0800

    seems to work

--- functions/shares.js
+++ functions/shares.js
@@ -5,17 +5,17 @@ const { ACCESS_TOKEN } = process.env;
 const API_URL = 'https://api.netlify.com/api/v1/';
 
 async function handleShares(event, context, callback) {
-	console.debug(`🦄 shares.js`);
+	//console.debug(`🦄 shares.js`);
 
 	try {
 		const formsRes = await fetch(`${API_URL}forms?access_token=${ACCESS_TOKEN}`);
-		console.debug(`🌈`, formsRes);
+		//console.debug(`🌈`, formsRes);
 		const formsData = await formsRes.json();
-		console.debug(`🔥`, formsData);
+		//console.debug(`🔥`, formsData);
 		const subsRes = await fetch(`${API_URL}sites/${formsData[0].site_id}/submissions?access_token=${ACCESS_TOKEN}`);
-		console.debug(`😈`, subsRes);
+		//console.debug(`😈`, subsRes);
 		const subsData = await subsRes.json();
-		console.debug(`🧜‍♀️`, subsData);
+		//console.debug(`🧜‍♀️`, subsData);
 		return {
 			statusCode: 200,
 			body: JSON.stringify(subsData),
