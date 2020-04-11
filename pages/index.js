ed6770ac632e7bbf38684365ff1f461df80494ae
Jesse Hattabaugh <arkanciscan@gmail.com>
2020-04-11 15:55:20 -0700

    hello resume

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
-export default () => <h1>Hello Next.js</h1>;
+export default Index;
+
+import Link from 'next/link';
+
+function Index() {
+	return (
+		<main>
+			<h1>Jesse Hattabaugh's Website</h1>
+			<nav>
+				<Link href="/resume">
+					<a title="Let's Work Together!">Resume</a>
+				</Link>
+			</nav>
+		</main>
+	);
+}
---
+++ pages/resume.js
@@ -0,0 +1,11 @@
+export default Resume;
+
+function Resume() {
+	return (
+		<main>
+			<h1>Resume</h1>
+
+			<p>When I was a Junior in High School I bought a book about Macromedia Dreamweaver and Fireworks.  A few months later I walked into an ad agency and asked if they needed a web developer.  They called me back to my surprise, and I got my first job. The rest is history.</p>
+		</main>
+	);
+}
