ca532bf0df737391eed98bd60d76e0629f54d6ce
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2022-11-10 14:43:18 -0800

    service worker tries to start but can't from a subdir

--- app/bundles/sw.mjs
+++ app/bundles/sw.mjs
@@ -1,1 +1,8 @@
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+registerRoute(() => {
+	return true;
+}, new StaleWhileRevalidate());
+
 console.debug('👋👷‍♂️ service worker loaded');
