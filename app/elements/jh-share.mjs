8af86de9dc860c405e8b5f939d605b886c3b95f5
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2022-12-26 13:44:00 -0800

    stuf

--- app/elements/jh-share.mjs
+++ app/elements/jh-share.mjs
@@ -1,6 +1,6 @@
 /**  @type {import('@enhance/types').EnhanceElemFn} */
 export default function ({ html, state }) {
-	const { title, text, url } = state.store;
+	const { title, text, url, error } = state.store;
 	return html`<style>
 			label,
 			input,
@@ -10,16 +10,17 @@ export default function ({ html, state }) {
 				display: block;
 			}
 			[type='submit'] {
+				border-radius: 0.25em;
+				border: 1px outset silver;
 				font-size: 1.5em;
 				padding: 0.5em;
-				border-radius: 0.25em;
 			}
 		</style>
-		<form method="post" enctype="multipart/form-data" action="/share">
+		<form action="/share">
+			${error}
 			<label>title <input name="title" type="text" value="${title}" /></label>
 			<label>text <textarea name="text" type="text">${text}</textarea></label>
 			<label>url <input name="url" type="url" value="${url}" /></label>
-			<label>photo <input name="photo" type="file" accept="image/jpg" /></label>
 			<input type="submit" value="share" />
 		</form>`;
 }
