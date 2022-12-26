e6f49934ed8bc420a9364271fda25309d5865e4e
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2022-12-26 13:44:00 -0800

    stuf

--- app/api/share.mjs
+++ app/api/share.mjs
@@ -1,13 +1,20 @@
 /** @type {import('@enhance/types').EnhanceApiFn} */
 export async function post(req) {
 	console.log('🌋', req.body);
-	if (req.body) {
+	const { test, title, url } = req.body;
+	if (test || title || url) {
 		return { location: '/thanks' };
+	} else {
+		return {
+			session: { error: 'you have to share something' },
+			location: '/share',
+		};
 	}
-
 }
 
 /** @type {import('@enhance/types').EnhanceApiFn} */
 export async function get(req) {
-	return { json: req.query };
+	const { test, title, url } = req.query;
+	const { error, ...session } = req.session;
+	return { json: { test, title, url, error }, session };
 }
