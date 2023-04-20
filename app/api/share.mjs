9672c1a49f8fb000746535e15ff3f8ba9084e7eb
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2023-04-20 13:14:38 -0700

    add image file to shares

--- app/api/share.mjs
+++ app/api/share.mjs
@@ -1,26 +1,50 @@
+import crypto from 'crypto';
+
 import arc from '@architect/functions';
-import { v4 as uuid } from 'uuid';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import lms from 'lambda-multipart-parser';
 
 /** @type {import('@enhance/types').EnhanceApiFn} */
 export async function post(req) {
 	const { session } = req;
 	const { isAuthorized } = session;
-	const { text, title, url } = req.body;
-	if (text || title || url) {
-		// send the share somewhere
+
+	// @see https://begin.com/blog/posts/2023-02-08-upload-files-in-forms-part-1#decoding-the-multipart-form-data
+	// @ts-ignore
+	const form = await lms.parse({ ...req, body: req.rawBody });
+	const { text, title, url } = form;
+	const [image] = form.files;
+
+	if (text || title || url || image) {
 		try {
 			const db = await arc.tables();
-			const shareId = uuid();
+			const shareId = crypto.randomUUID();
 			const createdAt = Date.now();
+
+			let imageKey;
+			if (image) {
+				console.log('💿', image);
+				const { content: Body, filename } = image;
+				const { ARC_STATIC_BUCKET: Bucket, AWS_REGION: region } = process.env;
+				imageKey = `.shared/${shareId}/${filename}`;
+				const client = new S3Client({ region });
+				const params = { Bucket, Key: imageKey, Body };
+				const command = new PutObjectCommand(params);
+				await client.send(command);
+			}
+
 			const result = await db.shares.put({
 				createdAt,
+				image: imageKey,
 				isAuthorized,
 				shareId,
 				text,
 				title,
 				url,
 			});
-			console.log('💌', 'share saved', result);
+
+			console.log('💌 share saved', result);
+
 			return {
 				location: '/thanks',
 			};
@@ -31,13 +55,12 @@ export async function post(req) {
 				location: '/share',
 			};
 		}
-	} else {
-		console.log('🌋', { text, title, url });
-		return {
-			session: { error: 'you have to share something' },
-			location: '/share',
-		};
 	}
+	// console.log('🌋', req);
+	return {
+		session: { error: 'you have to share something' },
+		location: '/share',
+	};
 }
 
 /** @type {import('@enhance/types').EnhanceApiFn} */
