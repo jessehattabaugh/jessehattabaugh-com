8983200fac8a4b1e5f578d9e04a7d037f1e34ee0
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2024-02-06 15:40:15 -0800

    refactor HyperStatus

--- app/browser/HyperStatus.mjs
+++ app/browser/HyperStatus.mjs
@@ -4,10 +4,7 @@
 export class HyperStatus extends HTMLElement {
 	constructor() {
 		super();
-		this.handleFetchStart = this.handleFetchStart.bind(this);
-		this.handleFetchSuccess = this.handleFetchSuccess.bind(this);
-		this.handleFetchError = this.handleFetchError.bind(this);
-		//this.handleFetchEnd = this.handleFetchEnd.bind(this);
+		this.toggleStatusSlots = this.toggleStatusSlots.bind(this);
 		// console.debug('🛠️ HyperStatus constructed');
 	}
 
@@ -15,96 +12,48 @@ export class HyperStatus extends HTMLElement {
 		this.for = (this.getAttribute('for') || '').split(',').map((id) => {
 			return id.trim();
 		});
-		document.addEventListener('hyper-fetch-start', this.handleFetchStart);
-		document.addEventListener('hyper-fetch-success', this.handleFetchSuccess);
-		document.addEventListener('hyper-fetch-error', this.handleFetchError);
-		//document.addEventListener('hyper-fetch-end', this.handleFetchEnd);
+		document.addEventListener('hyper-fetch-start', this.toggleStatusSlots);
+		document.addEventListener('hyper-fetch-success', this.toggleStatusSlots);
+		document.addEventListener('hyper-fetch-error', this.toggleStatusSlots);
 		//console.debug('🔗 HyperStatus event listeners added', { for: this.for });
 	}
 
 	disconnectedCallback() {
-		document.removeEventListener('hyper-fetch-start', this.handleFetchStart);
-		document.removeEventListener('hyper-fetch-success', this.handleFetchSuccess);
-		document.removeEventListener('hyper-fetch-error', this.handleFetchError);
-		//document.removeEventListener('hyper-fetch-end', this.handleFetchEnd);
+		document.removeEventListener('hyper-fetch-start', this.toggleStatusSlots);
+		document.removeEventListener('hyper-fetch-success', this.toggleStatusSlots);
+		document.removeEventListener('hyper-fetch-error', this.toggleStatusSlots);
 		//console.debug('🧹 HyperStatus event listeners removed', {});
 	}
 
-	/**
-	 * Handles the start of a fetch operation.
-	 * @param {CustomEvent} event - The event triggered on fetch start.
+	/** Toggles slot visibility based on the event type and the id of the fetch operation.
+	 * @param {CustomEvent} event - The event triggered on fetch start, success, or error.
 	 */
-	handleFetchStart(event) {
+	toggleStatusSlots(event) {
 		const { id } = event.detail;
 		if (this.for.includes(id)) {
-			// console.debug('⏳ HyperStatus handling fetch start', { id });
-			this.updateVisibility(id, 'loading');
-		}
-	}
+			// get the type of event
+			const [, , type] = event.type.split('-');
+			//console.debug('⌛ HyperStatus handling event', { id, type });
 
-	/**
-	 * Handles the successful completion of a fetch operation.
-	 * @param {CustomEvent} event - The event triggered on fetch success.
-	 */
-	handleFetchSuccess(event) {
-		const { id } = event.detail;
-		if (this.for.includes(id)) {
-			// console.debug('✅ HyperStatus handling fetch success', { id });
-			this.updateVisibility(id, 'success');
-		}
-	}
+			// hide all status slots
+			['loading', 'success', 'error'].forEach((status) => {
+				/** @type {HTMLElement | null} */
+				const slot = this.querySelector(`[slot="${status}"]`);
+				if (slot) {
+					slot.style.display = 'none';
+					//console.debug('🙈 HyperStatus hid', { id, type });
+				}
+			});
 
-	/**
-	 * Handles an error in a fetch operation.
-	 * @param {CustomEvent} event - The event triggered on fetch error.
-	 */
-	handleFetchError(event) {
-		const { id } = event.detail;
-		if (this.for.includes(id)) {
-			// console.debug('⛔ HyperStatus handling fetch error', { id });
-			this.updateVisibility(id, 'error');
-		}
-	}
-
-	/**
-	 * Handles the end of a fetch operation. This might be optional depending on whether specific actions are needed on fetch end.
-	 * @param {CustomEvent} event - The event triggered on fetch end.
-	 *
-	handleFetchEnd(event) {
-		const { id } = event.detail;
-		if (this.targets.includes(id)) {
-			console.debug('🏁 HyperStatus handling fetch end', { id });
-		}
-	}*/
-
-	/**
-	 * Updates the visibility of slots based on the current status.
-	 * @param {string} id - The identifier of the HXFetch element.
-	 * @param {string} status - The current fetch status.
-	 */
-	updateVisibility(id, status) {
-		if (this.for.includes(id)) {
-			this.hideAllSlots();
+			// update slots
 			/** @type {HTMLElement | null} */
-			const slot = this.querySelector(`[slot="${status}"]`);
+			const slot = this.querySelector(`[slot="${type}"]`);
 			if (slot) {
 				slot.style.display = 'revert';
+				//console.debug('🔊 HyperStatus showed', { id, type });
+			} else {
+				//console.debug('🤷 HyperStatus no slot for type', { id, type });
 			}
-			// console.debug('🔄 HyperStatus updating visibility', { id, status });
 		}
-	}
-
-	/**
-	 * Hides all slot elements.
-	 */
-	hideAllSlots() {
-		['loading', 'success', 'error'].forEach((status) => {
-			/** @type {HTMLElement | null} */
-			const slot = this.querySelector(`[slot="${status}"]`);
-			if (slot) {
-				slot.style.display = 'none';
-			}
-		});
-		// console.debug('🙈 HyperStatus hiding all slots', {});
 	}
 }
