a77d7bcd59ec9a05f6e8c74d90bea3ff2a92bd7e
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2024-02-07 17:19:37 -0800

    implement HyperUpdate

--- app/browser/HyperElements.mjs
+++ app/browser/HyperElements.mjs
@@ -1,7 +1,9 @@
 import { HyperLink } from './HyperLink.mjs';
 import { HyperForm } from './HyperForm.mjs';
 import { HyperStatus } from './HyperStatus.mjs';
+import { HyperUpdate } from './HyperUpdate.mjs';
 
 customElements.define('hyper-link', HyperLink);
 customElements.define('hyper-form', HyperForm);
 customElements.define('hyper-status', HyperStatus);
+customElements.define('hyper-update', HyperUpdate);
---
+++ app/browser/HyperUpdate.mjs
@@ -0,0 +1,87 @@
+import { HyperStatus } from './HyperStatus.mjs';
+/**
+ * A custom element that updates it's content based on the response of a fetch operation.
+ */
+export class HyperUpdate extends HyperStatus {
+	constructor() {
+		super();
+		this.update = this.update.bind(this);
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+		this.id = this.getAttribute('id');
+		this.record = this.getAttribute('record') || true;
+		this.how = this.getAttribute('how') || 'replace';
+		this.select = this.getAttribute('select') || `#${this.id}`;
+		this.addEventListener('hyper-fetch-end', this.update);
+	}
+
+	disconnectedCallback() {
+		this.removeEventListener('hyper-fetch-end', this.update);
+		super.disconnectedCallback();
+	}
+
+	/**
+	 * @param {CustomEvent<import('../../types').FetchDetails>} event
+	 */
+	async update(event) {
+		try {
+			const { detail } = event;
+			const { data, url } = detail;
+			const { select } = this.select;
+
+			// Parse the response and update the content
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(data, 'text/html');
+			const newContent = doc.querySelector(select);
+
+			const cause = { data, doc, select, url };
+			console.debug('🔄 HyperContent update', cause);
+
+			if (newContent) {
+				switch (this.how) {
+					case 'append':
+						await this.transitionView(() => {
+							return this.appendChild(newContent);
+						});
+						break;
+					case 'prepend':
+						await this.transitionView(() => {
+							this.insertBefore(newContent, this.firstChild);
+						});
+						break;
+					case 'replace':
+					default:
+						await this.transitionView(() => {
+							this.innerHTML = newContent.innerHTML;
+						});
+						break;
+				}
+			} else {
+				throw new Error('🤬 HyperUpdate: no content matches the selector.', { cause });
+			}
+		} catch ({ message, cause }) {
+			console.error(message, cause);
+		}
+	}
+
+	/**
+	 * @param {function} updateContent - A function that updates the content of the element.
+	 */
+	async transitionView(updateContent) {
+		// Use the View Transitions API if available
+		if ('documentTransition' in document) {
+			try {
+				await document.documentTransition.prepare();
+				updateContent();
+				await document.documentTransition.start();
+			} catch (error) {
+				throw new Error('👹 HyperUpdate: Error transitioning view', error);
+			}
+		} else {
+			// View Transitions API is not supported so update the content directly
+			updateContent();
+		}
+	}
+}
