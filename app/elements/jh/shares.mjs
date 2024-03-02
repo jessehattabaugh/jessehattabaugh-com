f30d4b90d310e13242725d42b0d6a7fbe9ddb980
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2024-03-02 14:49:30 -0800

    make now page more appropriate

--- app/elements/jh/shares.mjs
+++ app/elements/jh/shares.mjs
@@ -1,13 +1,12 @@
 /**  @type {import('@enhance/types').EnhanceElemFn} */
 export default function ({ html, state }) {
 	const { shares = [], error } = state.store;
-	// console.debug('🪺', { shares, error });
+	const items = shares.map((/** @type {Object} */ _, /** @type {Number} */ i) => {
+		return `<li><jh-share index=${i}></jh-share></li>`;
+	});
+	// console.debug('🪺', { error, items, shares });
 	return html`${error && `<div class="error">${error}</div>`}
-		<ol>
-			${shares
-				.map((/** @type {Object} */ _, /** @type {Number} */ i) => {
-					return `<li><jh-share index=${i}></jh-share></li>`;
-				})
-				.join('')}
-		</ol>`;
+	<ol>
+		${items ? items.join('') : html`<li>🤷No shares</li>`}
+	</ol>`;
 }
