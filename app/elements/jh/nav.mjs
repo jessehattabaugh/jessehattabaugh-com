5a0a1460db72abb499ce00523ae2036f38da414b
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2024-03-03 13:02:31 -0800

    making the picture element a figure for reasons

--- app/elements/jh/nav.mjs
+++ app/elements/jh/nav.mjs
@@ -6,8 +6,8 @@ export default function ({ html }) {
 			<li><a href="/about">About</a></li>
 			<li><a href="/ideas">Ideas</a></li>
 			<li><a href="/now">Now</a></li>
-			<li><a href="/contact">Contact</a></li>
 			<li><a href="/resume">Resume</a></li>
+			<li><a href="/contact">Contact</a></li>
 		</ul>
 	</nav>`;
 }
