7351e4c568f8614d2e1e8032ee1e117f043a3d74
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-02-16 16:46:03 -0800

    making an element

---
+++ app/elements/ideas/integerScaling.mjs
@@ -0,0 +1,6 @@
+/** @type {import('@enhance/types').EnhanceElemFn} */
+export default function ({ html, state }) {
+	const { store } = state;
+
+	return html`
+}
--- app/elements/jh/auth.mjs
+++ app/elements/jh/auth.mjs
@@ -2,10 +2,9 @@
 export default function ({ html, state }) {
 	const { store } = state;
 	const { isAuthorized } = store;
-	switch (isAuthorized) {
-		case true:
-			return html`<a href="/logout">Unauthorize</a>`;
-		default:
-			return html`<a href="/auth">Authorize</a>`;
-	}
+	return html`<form>
+		<label>Resolution Width (in pixels): <input type="number" name="width" min="1">px</label>
+		<label>Resolution Height (in pixels): <input type="number" name="height" min="1">px</label>
+		<label>Screen size (measured diagonally, in inches): <input type="number" name="size" min="1">in</label>
+	</form>`;
 }
