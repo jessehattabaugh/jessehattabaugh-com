2967c389dffc78e74d7fe0b730aa4c7d001324e0
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-03-01 20:07:13 -0800

    add walls

--- app/browser/matter.mjs
+++ app/browser/matter.mjs
@@ -1,34 +1,62 @@
 import Matter from 'matter-js';
 
 // module aliases
-var Engine = Matter.Engine,
+const Engine = Matter.Engine,
 	Render = Matter.Render,
 	Runner = Matter.Runner,
 	Bodies = Matter.Bodies,
-	Composite = Matter.Composite;
+	Composite = Matter.Composite,
+	Constraint = Matter.Constraint;
+
+// create a canvas
+const canvasSize = 3840;
+const canvas = document.createElement('canvas');
+canvas.width = canvasSize;
+canvas.height = canvasSize;
+document.body.appendChild(canvas);
 
 // create an engine
-var engine = Engine.create();
+const engine = Engine.create({ gravity: { x: 0, y: 0 } });
 
 // create a renderer
-var render = Render.create({
-	element: document.body,
-	engine: engine,
+const render = Render.create({
+	canvas,
+	engine,
+	options: { width: canvasSize, height: canvasSize },
 });
+// add walls to the world
+Composite.add(engine.world, createWalls(canvasSize));
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+/** @todo add more things here */
 
 // run the renderer
 Render.run(render);
 
 // create runner
-var runner = Runner.create();
+const runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+/**
+ * creates walls on each side of the canvas
+ * @param {number} canvasSize
+ */
+function createWalls(canvasSize) {
+	const opts = { isStatic: true };
+	const size = 10;
+	const halfWall = size / 2;
+	const midCanvas = canvasSize / 2;
+	const walls = [];
+
+	// north wall
+	walls.push(Bodies.rectangle(midCanvas, -halfWall, canvasSize, size, opts));
+	// south wall
+	walls.push(Bodies.rectangle(midCanvas, canvasSize + halfWall, canvasSize, size, opts));
+	// east wall
+	walls.push(Bodies.rectangle(-halfWall, midCanvas, size, canvasSize, opts));
+	// west wall
+	walls.push(Bodies.rectangle(canvasSize + halfWall, midCanvas, size, canvasSize, opts));
+
+	return walls;
+}
