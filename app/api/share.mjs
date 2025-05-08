109c44075f07550f07a81025d5a8f5058e96c3f2
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-05-08 14:48:10 -0700

    delete everything

--- app/api/share.mjs
+++
@@ -1,9 +0,0 @@
-
-/** move data from session to state.store
- * @type {import('@enhance/types').EnhanceApiFn} */
-export async function get(request) {
-	const { text = '', title = '', url = '' } = request.query;
-	const { error, isAuthorized } = request.session;
-	return { json: { error, isAuthorized, text, title, url } };
-}
-
