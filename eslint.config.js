02098429611be2d76cb21d647b33aa08d77b8786
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-05-08 17:29:33 -0700

    ensuring that eslint-plugin-compat gets the browser support memo

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,15 @@
 import compatPlugin from 'eslint-plugin-compat';
 import { defineConfig } from 'eslint/config';
+import { fileURLToPath } from 'node:url';
 import globals from 'globals';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
-import path from 'node:path';
 import prettierConfig from 'eslint-config-prettier/flat';
 import promisePlugin from 'eslint-plugin-promise';
 import sonarPlugin from 'eslint-plugin-sonarjs';
 import unicornPlugin from 'eslint-plugin-unicorn';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
 	includeIgnoreFile(gitignorePath),
@@ -38,6 +36,9 @@ export default defineConfig([
 				ecmaFeatures: {
 					impliedStrict: true,
 				},
+			},
+			settings: {
+				browsers: ['> 5%'],
 			},
 			sourceType: 'module',
 		},
