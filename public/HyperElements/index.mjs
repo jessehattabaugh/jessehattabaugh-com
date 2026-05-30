4f047c30715a65a1e5c6b91e10d5e9c65897ced2
Jesse Hattabaugh <arkanciscan@gmail.com>
2026-05-30 16:08:11 -0700

    blank slate

--- public/HyperElements/index.mjs
+++
@@ -1,9 +0,0 @@
-import { HyperLink } from './HyperLink.mjs';
-import { HyperForm } from './HyperForm.mjs';
-import { HyperStatus } from './HyperStatus.mjs';
-import { HyperUpdate } from './HyperUpdate.mjs';
-
-customElements.define('hyper-link', HyperLink);
-customElements.define('hyper-form', HyperForm);
-customElements.define('hyper-status', HyperStatus);
-customElements.define('hyper-update', HyperUpdate);
