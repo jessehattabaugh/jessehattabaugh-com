48ae279e8144e9f17abc1f70f7c9d0b42f9bf9ea
Jesse Hattabaugh <arkanciscan@gmail.com>
2026-05-30 16:08:11 -0700

    blank slate

--- app/elements/demo/books/add.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('@enhance/types').EnhanceElemFn} */
-export default function ({ html }) {
-	// prettier-ignore
-	return html`<hyper-status from="addBook">
-	<span slot="loading" style="display: none;">adding book...</span>
-	<span slot="success" style="display: none;">book added!</span>
-	<span slot="error" style="display: none;">error adding book!</span>
-</hyper-status>`;
-}
